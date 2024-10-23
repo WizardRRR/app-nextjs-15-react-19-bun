440c884d6ffd2fcbfc5f7b91008551bdb90d73cb
Royer Rutti Ramos <ruttiramosroyer@gmail.com>
2024-10-22 19:20:25 -0500

    feat: :art: format code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import styles from "./page.module.css";
+import styles from './page.module.css'
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Hello World!</h1>
+      <h1 className={styles.title}>Hello, Next.js!</h1>
+      <p className={styles.description}>
+        Get started by editing <code>app/page.tsx</code>
+      </p>
     </div>
-  );
+  )
 }
